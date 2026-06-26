1356ba2bd343c9f110a511a80460ebb8032e50a2
kinlay0 <egormoskaev4@gmail.com>
2026-06-26 15:06:24 +0300

    exclude domains

--- parser/parser.py
+++ parser/parser.py
@@ -1,8 +1,13 @@
 import requests
 
-url = "https://raw.githubusercontent.com/kinlay0/IPSets-For-Bypass-in-Russia/refs/heads/main/%D0%A0%D0%B0%D0%B7%D0%B1%D0%BB%D0%BE%D0%BA%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0%20%D0%BC%D0%BD%D0%BE%D0%B6%D0%B5%D1%81%D1%82%D0%B2%D0%B0%20%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81%D0%BE%D0%B2(%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%20-%20ChatGPT)/hosts"
+url = ["https://raw.githubusercontent.com/kinlay0/IPSets-For-Bypass-in-Russia/refs/heads/main/%D0%A0%D0%B0%D0%B7%D0%B1%D0%BB%D0%BE%D0%BA%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0%20%D0%BC%D0%BD%D0%BE%D0%B6%D0%B5%D1%81%D1%82%D0%B2%D0%B0%20%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81%D0%BE%D0%B2(%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%20-%20ChatGPT)/hosts"]
 
 url_block = ["https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro-onlydomains.txt"]
+
+
+domains_exclude = [
+    "yougame.biz",
+]
 
 
 def download(link):
@@ -17,7 +22,11 @@ def download(link):
 
 
 print("скачиваю файл...")
-lines = download(url)
+lines = []
+for u in url:
+    lines = download(u)
+    if lines:
+        break
 print("скачал, строк:", len(lines))
 
 if not lines:
@@ -49,7 +58,7 @@ for line in lines:
     domain = domain.split('#')[0].strip()
 
     if ip == '0.0.0.0':
-        if domain not in block_seen:
+        if domain not in block_seen and domain not in domains_exclude:
             block_seen.add(domain)
             block_domains.append(domain)
     else:
@@ -65,7 +74,7 @@ for b_url in url_block:
     added = 0
     for line in b_lines:
         domain = line.strip().split('#')[0].strip()
-        if domain and domain not in block_seen:
+        if domain and domain not in block_seen and domain not in domains_exclude:
             block_seen.add(domain)
             block_domains.append(domain)
             added += 1
